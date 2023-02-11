de323d141e9affeb8c6998b4d8dc3b49f2497ebf
Crisley Linhares <prestes40@gmail.com>
2023-02-11 13:34:01 -0400

    Aula 25

--- src/main/java/br/com/linhares/crisley/appium/core/BaseTest.java
+++ src/main/java/br/com/linhares/crisley/appium/core/BaseTest.java
@@ -1,17 +1,38 @@
 package br.com.linhares.crisley.appium.core;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
+
+    @Rule
+    public TestName testName = new TestName();
 
     @AfterClass
     public static void finalizaClasse(){
         DriverFactory.killDriver();
     }
 
+    public void gerarScreenshot(){
+        File imagem = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(imagem, new File("target/screenshots/"+testName.getMethodName()+".png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @After
     public void tearDown(){
+        gerarScreenshot();
         DriverFactory.getDriver().resetApp();
     }
 }
