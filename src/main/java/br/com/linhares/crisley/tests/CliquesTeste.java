545d6b4636a36c17ef6777828a479879fe3a074e
Crisley Linhares <prestes40@gmail.com>
2023-02-16 13:38:21 -0400

    Aula 36

--- src/main/java/br/com/linhares/crisley/tests/CliquesTeste.java
+++ src/main/java/br/com/linhares/crisley/tests/CliquesTeste.java
@@ -4,6 +4,7 @@ import br.com.linhares.crisley.appium.core.BaseTest;
 import br.com.linhares.crisley.pages.CliquesPage;
 import br.com.linhares.crisley.pages.MenuPage;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CliquesTeste extends BaseTest {
@@ -11,10 +12,21 @@ public class CliquesTeste extends BaseTest {
     private CliquesPage cliques = new CliquesPage();
     private MenuPage menu = new MenuPage();
 
+    @Before
+    public void setup(){
+        menu.acessarCliques();
+    }
+
     @Test
     public void deveRealizarCliqueLongo(){
-        menu.acessarCliques();
         cliques.cliqueLongo();
         Assert.assertEquals("Clique Longo", cliques.obterTextoCampo());
+    }
+
+    @Test
+    public void deveRealizarCliqueDuplo(){
+        cliques.clicarPorTexto("Clique duplo");
+        cliques.clicarPorTexto("Clique duplo");
+        Assert.assertEquals("Duplo Clique", cliques.obterTextoCampo());
     }
 }
