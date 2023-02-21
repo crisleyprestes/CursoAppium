621a2654454c54079c5de8a9cce727a134c07421
Crisley Linhares <prestes40@gmail.com>
2023-02-20 21:13:24 -0400

    Aula 55

--- src/main/java/br/com/linhares/crisley/appium/core/DriverFactory.java
+++ src/main/java/br/com/linhares/crisley/appium/core/DriverFactory.java
@@ -3,6 +3,7 @@ package br.com.linhares.crisley.appium.core;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -16,7 +17,7 @@ public class DriverFactory{
 
     public static AndroidDriver<MobileElement> getDriver(){
         if(driver == null){
-            createDriver();
+            createTestObjectDriver();
         }
         return driver;
     }
@@ -44,14 +45,22 @@ public class DriverFactory{
     }
 
     private static void createTestObjectDriver(){
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("platformName", "Android");
+        // caps.setCapability("appium:platformVersion", "11");
+        // caps.setCapability("appium:deviceName", "Samsung.*Galaxy.*");
+        // caps.setCapability("appium:orientation", "portrait");
+        caps.setCapability("appium:app", "storage:filename=CTAppium_2_0.apk");
+        caps.setCapability("automationName", "UiAutomator2");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", "oauth-prestes40-5f4f0");
+        sauceOptions.setCapability("accessKey", "12eff6f9-d01e-429c-bb06-1ef17559a4cb");
+        caps.setCapability("sauce:options", sauceOptions);
 
         try {
             URL url = new URL("https://oauth-prestes40-5f4f0:" +
                     "12eff6f9-d01e-429c-bb06-1ef17559a4cb@ondemand.us-west-1.saucelabs.com:443/wd/hub");
-            driver = new AndroidDriver(url, desiredCapabilities);
+            driver = new AndroidDriver(url, caps);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
